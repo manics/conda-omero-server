b40a8930d10d0e99aeaa76ae2086b1da2e555876
Simon Li <orpheus+devel@gmail.com>
2020-01-19 22:59:57 +0000

    5.6.0 Python 3

--- build.sh
+++ build.sh
@@ -2,16 +2,25 @@
 
 set -eux
 OMERODIR="${PREFIX}/opt/omero/server/OMERO.server"
-
-# Use the decoupled omero-py
-rm -rf lib/python bin/
+CONFIGDIR="${PREFIX}/opt/omero/server/config"
 
 mkdir -p "${OMERODIR}"
 mv * "${OMERODIR}"
 
+# Symlink duplicate jars
+pushd "${OMERODIR}/lib"
+for f in client/*; do
+    if [ -f "server/${f#*/}" ]; then
+        ln -sf "../server/${f#*/}" "./client/${f#*/}"
+    fi
+done
+popd
+
+mkdir -p "$PREFIX/bin" "${OMERODIR}/etc/grid" "${CONFIGDIR}"
+
 # Empty directories aren't packaged
 echo "$PKG_NAME $PKG_VERSION $PKG_BUILDNUM" > "${OMERODIR}/etc/grid/.conda-build"
+echo "$PKG_NAME $PKG_VERSION $PKG_BUILDNUM" > "${CONFIGDIR}/.conda-build"
 
-mkdir -p $PREFIX/bin
 cp $RECIPE_DIR/omero-server $PREFIX/bin/
 chmod +x $PREFIX/bin/omero-server
